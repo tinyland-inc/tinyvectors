ec7be6c166b1ca43701942394046ff2a2d2121c4
John Sullivan <jsullivan2@users.noreply.github.com>
2026-02-01 13:47:35 -0500

    chore: checkpoint status on yoga feb 1

--- dist/motion/index.js
+++
@@ -1,6 +0,0 @@
-import { DeviceMotion as r } from "./DeviceMotion.js";
-import { ScrollHandler as i } from "./ScrollHandler.js";
-export {
-  r as DeviceMotion,
-  i as ScrollHandler
-};
--- dist/svelte/index.js
+++
@@ -1,6 +0,0 @@
-import r from "./BlobSVG.js";
-import t from "./TinyVectors.js";
-export {
-  r as BlobSVG,
-  t as TinyVectors
-};
